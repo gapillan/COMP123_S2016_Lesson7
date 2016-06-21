b8addef9c29856bb5a3f400bf8e8f3287537c64f
gapillan <gapillan@my.centennialcollege.ca>
2016-06-21 14:04:34 -0400

    Created a random Dice number

--- COMP123_S2016_Lesson7/Program.cs
+++ COMP123_S2016_Lesson7/Program.cs
@@ -10,6 +10,15 @@ namespace COMP123_S2016_Lesson7
     {
         static void Main(string[] args)
         {
+            Random random = new Random(); // pseudo number object 
+
+            int firstDie = random.Next(1, 7);
+            int secondDie = random.Next(1, 7);
+            int dice = firstDie + secondDie;
+
+            Console.WriteLine("First: {0}", firstDie);
+            Console.WriteLine("Second: {0}", secondDie);
+            Console.WriteLine("Result: {0}", dice);
         }
     }
 }
