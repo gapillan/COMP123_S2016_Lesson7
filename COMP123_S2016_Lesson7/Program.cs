3870f868bbe704d4ba99e92bf93ceac32526be09
gapillan <gapillan@my.centennialcollege.ca>
2016-06-21 14:08:16 -0400

    Creating a List method

--- COMP123_S2016_Lesson7/Program.cs
+++ COMP123_S2016_Lesson7/Program.cs
@@ -11,14 +11,20 @@ namespace COMP123_S2016_Lesson7
         static void Main(string[] args)
         {
             Random random = new Random(); // pseudo number object 
+            List<int> diceRolls = new List<int>();
+            int maxRolls = 50;
 
-            int firstDie = random.Next(1, 7);
-            int secondDie = random.Next(1, 7);
-            int dice = firstDie + secondDie;
+            for (int i = 0; i < maxRolls; i++)
+            {
+                 int firstDie = random.Next(1, 7);
+                 int secondDie = random.Next(1, 7);
+                 int dice = firstDie + secondDie;
+                 diceRolls.Add(dice);
+            }
 
-            Console.WriteLine("First: {0}", firstDie);
-            Console.WriteLine("Second: {0}", secondDie);
-            Console.WriteLine("Result: {0}", dice);
+
+           
+            
         }
     }
 }
